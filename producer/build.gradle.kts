31b79ce0093dd757522b8377406b957f60f86a94
lolmageap <ekxk1234@naver.com>
2024-10-04 22:53:24 +0900

    redis setting

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 
 dependencies {
     implementation(project(":common"))
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
