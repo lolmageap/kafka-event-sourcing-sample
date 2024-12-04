1eade4fa53b7bf60a29c33881988a7b1cdfb8247
lolmageap <ekxk1234@naver.com>
2024-12-04 23:38:01 +0900

    setting for redisson configuration

--- store/src/main/kotlin/com/example/store/config/RedisConfig.kt
+++ store/src/main/kotlin/com/example/store/config/RedisConfig.kt
@@ -1,6 +1,9 @@
 package com.example.store.config
 
 import com.example.store.property.RedisProperty
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisClusterConfiguration
@@ -23,4 +26,19 @@ class RedisConfig(
         RedisTemplate<String, String>().apply {
             connectionFactory = connectionFactory()
         }
+
+    @Bean
+    fun redisCluster(): RedissonClient {
+        val clusterConfig =
+            Config().apply {
+                useClusterServers()
+                    .addNodeAddress(redisProperty.nodes)
+                    .setScanInterval(2000)
+                    .setConnectTimeout(100)
+                    .setTimeout(3000)
+                    .setRetryAttempts(3)
+                    .setRetryInterval(1500)
+            }
+        return Redisson.create(clusterConfig)
+    }
 }
--- store/src/main/kotlin/com/example/store/property/RedisProperty.kt
+++ store/src/main/kotlin/com/example/store/property/RedisProperty.kt
@@ -1,8 +1,16 @@
 package com.example.store.property
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.data.redis.connection.RedisNode
 
 @ConfigurationProperties(prefix = "spring.data.redis.cluster")
 data class RedisProperty(
     val nodes: String,
-)
+) {
+    val redisNodes: List<RedisNode>
+        get() =
+            nodes.split(",").map {
+                val (host, port) = it.split(":")
+                RedisNode(host, port.toInt())
+            }
+}
